39c8b4760e0f1cbb8a384292d3be3b0c5e796ba7
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-24 20:49:33 -0400

    Adding method GetAllAsync on ProductService

--- Services/CatalogService/Catalog.Application/Services/ProductService.cs
+++ Services/CatalogService/Catalog.Application/Services/ProductService.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Catalog.Application.DTOs;
+using Catalog.Application.Interfaces;
+using Catalog.Domain.Entities;
+
 namespace Catalog.Application.Services
 {
-    internal class ProductService
+    public class ProductService
     {
+        private readonly IProductRepository _repository;
+
+        private ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllAsync()
+        {
+            var products = await _repository.GetAllAsync();
+
+            return products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Stock = p.Stock
+            });
+        }
     }
 }
