842bf676959015b796f2d5c2f206ce80b6f6aae5
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-27 12:30:43 -0400

    Adding GetAllAsync method on OrderService

---
+++ Services/OrderingService/Ordering.Application/Services/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ordering.Application.DTOs;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Interfaces;
+
+namespace Ordering.Application.Services
+{
+    public class OrderService
+    {
+        private readonly IOrderRepository _repository;
+
+        public OrderService(IOrderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<OrderDto>> GetAllAsync()
+        {
+            var orders = await _repository.GetAllAsync();
+            return orders.Select(order => new OrderDto
+            {
+                Id = order.Id,
+                CustomerName = order.CustomerName,
+                Product = order.Product,
+                Quantity = order.Quantity,
+                TotalPrice = order.TotalPrice,
+                OrderDate = order.OrderDate,
+            });
+
+                
+
+
+        }
+    }
+}
