d9b66c1f7dc55695456f284b366ec41e8948e403
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-28 00:16:51 -0400

    Adding OrderController and GetAll method

---
+++ Services/OrderingService/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.DTOs;
+using Ordering.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: api/Order
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var orders = await _orderService.GetAllAsync();
+            return Ok(orders);
+        }
+
+    }
+}
