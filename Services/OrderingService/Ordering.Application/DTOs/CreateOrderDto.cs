b35ee27c09ae9bb808179db91aaeb44eb90372ac
andresmarz <andresmarquezzuleta@gmail.com>
2025-07-31 15:56:44 -0400

    Modifying CreateOrderDto for working with REST between Microservices

--- Services/OrderingService/Ordering.Application/DTOs/CreateOrderDto.cs
+++ Services/OrderingService/Ordering.Application/DTOs/CreateOrderDto.cs
@@ -2,9 +2,8 @@
 {
     public class CreateOrderDto
     {
+        public Guid Id { get; set; }
         public string CustomerName { get; set; } = string.Empty;
-        public string Product { get; set; } = string.Empty;
-        public int Quantity { get; set; }
-        public decimal TotalPrice { get; set; }
+        public int Quantity { get; set; }      
     }
 }
