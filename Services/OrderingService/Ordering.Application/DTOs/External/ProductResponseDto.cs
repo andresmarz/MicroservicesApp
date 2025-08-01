225d2bdaa521784481867225ddbee1e225690737
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-01 19:33:38 -0400

    Updating properties of ProductResponseDto

--- Services/OrderingService/Ordering.Application/DTOs/External/ProductResponseDto.cs
+++ Services/OrderingService/Ordering.Application/DTOs/External/ProductResponseDto.cs
@@ -9,10 +9,8 @@ namespace Ordering.Application.DTOs.External
     public class ProductResponseDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; } = null!;
         public decimal Price { get; set; }
-        public int Stock { get; set; }
     }
 
 }
