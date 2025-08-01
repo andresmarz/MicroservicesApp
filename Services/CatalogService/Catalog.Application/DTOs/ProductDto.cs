163854dce5fa1540bf83aa8515e104a4023d238d
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-01 12:41:47 -0400

    Modifying ProductDto for microservices REST communication

--- Services/CatalogService/Catalog.Application/DTOs/ProductDto.cs
+++ Services/CatalogService/Catalog.Application/DTOs/ProductDto.cs
@@ -9,10 +9,8 @@ namespace Catalog.Application.DTOs
     public class ProductDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
+        public string Name { get; set; } = string.Empty;        
+        public decimal Price { get; set; }        
     }
 }
 
