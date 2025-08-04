830746867e74dab14b0fa75740b6d61ca36f7156
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-04 11:39:49 -0400

    Fixing ProductDto after adding another Dto for REST communication

--- Services/CatalogService/Catalog.Application/DTOs/ProductDto.cs
+++ Services/CatalogService/Catalog.Application/DTOs/ProductDto.cs
@@ -9,8 +9,10 @@ namespace Catalog.Application.DTOs
     public class ProductDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;        
-        public decimal Price { get; set; }        
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
 
