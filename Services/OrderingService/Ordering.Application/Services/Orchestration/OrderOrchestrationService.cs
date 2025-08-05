9589eb199c9fb278d1ade32908721a2f9cd6db19
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-05 00:04:51 -0400

    Implementing OrderOrchestrationService on Ordering.Application.Services.Orchestration

--- Services/OrderingService/Ordering.Application/Services/Orchestration/OrderOrchestrationService.cs
+++ Services/OrderingService/Ordering.Application/Services/Orchestration/OrderOrchestrationService.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ordering.Application.DTOs.External;
+using Ordering.Application.Interfaces;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Interfaces;
 
 namespace Ordering.Application.Services.Orchestration
 {
-    internal class OrderOrchestrationService
+    public class OrderOrchestrationService : IOrderOrchestrationService
     {
+        private readonly ICatalogServiceHttpClient _catalogClient;
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderOrchestrationService(
+            ICatalogServiceHttpClient catalogClient,
+            IOrderRepository orderRepository)
+        {
+            _catalogClient = catalogClient;
+            _orderRepository = orderRepository;
+        }
+
+        public async Task CreateOrderFromCatalogAsync(Guid productId, int quantity)
+        {
+            var product = await _catalogClient.GetProductByIdAsync(productId);
+            if (product is null)
+                throw new Exception("Producto no encontrado en CatalogService.");
+
+            var totalPrice = product.Price * quantity;
+
+            var order = new Order
+            {
+                Id = Guid.NewGuid(),
+                ProductId = productId,
+                ProductName = product.Name,
+                Quantity = quantity,
+                TotalPrice = totalPrice,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            await _orderRepository.AddAsync(order);
+        }
     }
 }
