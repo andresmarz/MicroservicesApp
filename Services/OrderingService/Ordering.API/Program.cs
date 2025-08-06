e8c46040cbc903698f665bde519938c2272ee49c
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-06 16:41:56 -0400

    Fixing URL for HttpClient on Program.cs of Odering Microservice

--- Services/OrderingService/Ordering.API/Program.cs
+++ Services/OrderingService/Ordering.API/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddScoped<IOrderOrchestrationService, OrderOrchestrationService
 //Registering an HttpClient
 builder.Services.AddHttpClient<ICatalogServiceHttpClient, CatalogServiceHttpClient>(client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5146/");
+    client.BaseAddress = new Uri("http://localhost:5003/");
 });
 
 
