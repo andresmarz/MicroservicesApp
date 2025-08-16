f5499a107cc82c5da6f7868315aebae13387e145
andresmarz <andresmarquezzuleta@gmail.com>
2025-08-16 12:42:12 -0400

    Applying automated migrations at the inicialitation of the container for docker on Program.cs

--- Services/CatalogService/Catalog.API/Program.cs
+++ Services/CatalogService/Catalog.API/Program.cs
@@ -25,10 +25,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Apply automated migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
+app.UseSwagger();
     app.UseSwaggerUI();
 //}
 
